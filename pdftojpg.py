b3b6e67ebac8050e481698918fc87d117c796f33
Neha3112 <nehal.cs21@bmsce.ac.in>
2024-02-13 12:38:35 +0530

    graycode

--- pdftojpg.py
+++ pdftojpg.py
@@ -6,8 +6,8 @@ from PIL import Image
 import re
 import os
 
-pdf_path = 'C:\\Users\\Admin\\Major_project\\images\\6th Semester Provisional Result Sheet.pdf'
-output_folder = 'C:\\Users\\Admin\\Major_project\\images'
+pdf_path = 'C:\\Users\\Neha\\Documents\\Major_project\\6th Semester Provisional Result Sheet.pdf'
+output_folder = 'C:\\Users\\Neha\\Documents\\Major_project\\images'
 
 file = open(pdf_path, 'rb')
 readpdf = PyPDF2.PdfReader(file)
@@ -17,14 +17,14 @@ print('Page Count ==>', count)
 # Ensure the output folder exists
 os.makedirs(output_folder, exist_ok=True)
 
-with open('C:\\Users\\Admin\\Major_project\\path.txt', 'w') as file1:
+with open('C:\\Users\\Neha\\Documents\\Major_project\\path.txt', 'w') as file1:
     file1.write("\n")  # Add a newline for separation if needed
     a = 0
     while a < count:
         file1.write(output_folder + "\\image" + str(a) + ".jpg\n")
         a += 1
 
-images = convert_from_path(pdf_path, poppler_path=r'C:\Users\Admin\Downloads\Release-23.11.0-0\poppler-23.11.0\Library\bin')
+images = convert_from_path(pdf_path, poppler_path=r'C:\Users\Neha\Downloads\poppler-23.11.0\Library\bin')
 
 for i, image in enumerate(images):
     fname = f"{output_folder}\\image{i}.jpg"
